fd384133cce018c84f875b78c88e90614e3bcc92
Declan Byrd <declan.byrd@gmail.com>
2020-05-25 15:21:48 +0100

    feat: refactored readableDates filter

--- src/_filters/readableDates.js
+++ src/_filters/readableDates.js
@@ -1,32 +1,4 @@
-module.exports = (date, part) => {
-  const d = new Date(date);
-  if (part == "year") return d.getUTCFullYear();
-
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-
-  const suffix = {
-    1: "st",
-    2: "nd",
-    3: "rd",
-    21: "st",
-    22: "nd",
-    23: "rd",
-    31: "st"
-  };
-
-  return `${months[d.getMonth()]} ${d.getDate() +
-    (suffix[d.getDate()] || "th")} ${d.getUTCFullYear()}`;
+module.exports = (date) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(date).toLocaleDateString(undefined, options);
 };
