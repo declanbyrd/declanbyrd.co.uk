05b063e7309c083434e810e0f92ea1a4fd622d8b
Declan Byrd <declan.byrd@gmail.com>
2020-06-06 17:25:43 +0100

    feat: page transitions

--- .eleventy.js
+++ .eleventy.js
@@ -37,6 +37,7 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addPassthroughCopy('src/img');
   eleventyConfig.addPassthroughCopy('src/fonts');
+  eleventyConfig.addPassthroughCopy('src/scripts');
 
   return {
     dir: {
---
+++ src/scripts/animation.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', () => {
+  document.querySelector('body').classList.add('loaded');
+})
