1a93351d9d676d10f3e702729cf61c816cdf319d
Declan Byrd <declan.byrd@gmail.com>
2020-06-08 16:47:49 +0100

    feat: photo loading transition

--- src/assets/scripts/animation.js
+++ src/assets/scripts/animation.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', () => {
   document.querySelector('body').classList.add('loaded');
-})
+});
+
+document.querySelectorAll('.photo-item').forEach((photo) => {
+  photo.addEventListener('load', () => {
+    photo.classList.add('loaded');
+  });
+});
