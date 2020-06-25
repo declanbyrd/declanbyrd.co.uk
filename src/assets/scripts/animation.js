0025085529c1c5923d43e690ec98a97be4c9f256
Declan Byrd <declan.byrd@gmail.com>
2020-06-25 11:39:47 +0100

    fix: cleaned up animations

--- src/assets/scripts/animation.js
+++ src/assets/scripts/animation.js
@@ -1,7 +1,3 @@
-window.addEventListener('load', () => {
-  document.querySelector('body').classList.add('loaded');
-});
-
 document.querySelectorAll('.photo-item').forEach((photo) => {
   photo.addEventListener('load', () => {
     photo.classList.add('loaded');
