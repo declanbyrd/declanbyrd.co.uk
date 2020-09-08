719d512bf922887237259a707d89ffffdd930ed0
Declan Byrd <declan.byrd@gmail.com>
2020-09-08 22:29:51 +0100

    feat: light & dark themes, toggle icon

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,12 +1,56 @@
 'use strict';
 
-// Add a loaded class to photos triggering a fade in transition.
-const addPhotoTransitions = () => {
-  document.querySelectorAll('.photo-item').forEach((photo) => {
-    photo.classList.add('loaded');
-  });
+/**
+ * Dark mode toggle from Andy Bell
+ * https://hankchizljaw.com/wrote/create-a-user-controlled-dark-or-light-mode/
+ */
+
+document.documentElement.classList.remove('no-js');
+
+const STORAGE_KEY = 'user-color-scheme';
+const COLOR_MODE_KEY = '--color-mode';
+const toggle = document.getElementById('themeToggle');
+
+const getCSSCustomProp = propKey => {
+  let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
+
+  if (response.length) {
+    response = response.replace(/\"/g, '').trim();
+  }
+
+  return response;
 };
 
-window.addEventListener('load', () => {
-  addPhotoTransitions();
+const applySetting = passedSetting => {
+  let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
+  if (currentSetting) {
+    document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
+  }
+};
+
+const toggleSetting = () => {
+  let currentSetting = localStorage.getItem(STORAGE_KEY);
+
+  switch (currentSetting) {
+    case null:
+      currentSetting = getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
+      break;
+    case 'light':
+      currentSetting = 'dark';
+      break;
+    case 'dark':
+      currentSetting = 'light';
+      break;
+  }
+
+  localStorage.setItem(STORAGE_KEY, currentSetting);
+
+  return currentSetting;
+};
+
+document.getElementById('themeToggle').addEventListener('click', evt => {
+  evt.preventDefault();
+  applySetting(toggleSetting());
 });
+
+applySetting();
