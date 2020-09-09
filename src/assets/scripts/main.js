8ef0905e077560caf3533eeccc3f565147e02108
Declan Byrd <declan.byrd@gmail.com>
2020-09-09 20:49:14 +0100

    feat: font toggle for open dyslexic

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -7,12 +7,14 @@
 
 document.documentElement.classList.remove('no-js');
 
-const STORAGE_KEY = 'user-color-scheme';
+const COLOR_STORAGE_KEY = 'user-color-scheme';
 const COLOR_MODE_KEY = '--color-mode';
-const toggle = document.getElementById('themeToggle');
+const themeToggle = document.getElementById('themeToggle');
 
-const getCSSCustomProp = propKey => {
-  let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
+const getCSSCustomProp = (propKey) => {
+  let response = getComputedStyle(document.documentElement).getPropertyValue(
+    propKey
+  );
 
   if (response.length) {
     response = response.replace(/\"/g, '').trim();
@@ -21,19 +23,23 @@ const getCSSCustomProp = propKey => {
   return response;
 };
 
-const applySetting = passedSetting => {
-  let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
+const applySetting = (passedSetting) => {
+  let currentSetting = passedSetting || localStorage.getItem(COLOR_STORAGE_KEY);
   if (currentSetting) {
-    document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
+    document.documentElement.setAttribute(
+      'data-user-color-scheme',
+      currentSetting
+    );
   }
 };
 
 const toggleSetting = () => {
-  let currentSetting = localStorage.getItem(STORAGE_KEY);
+  let currentSetting = localStorage.getItem(COLOR_STORAGE_KEY);
 
   switch (currentSetting) {
     case null:
-      currentSetting = getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
+      currentSetting =
+        getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
       break;
     case 'light':
       currentSetting = 'dark';
@@ -43,14 +49,53 @@ const toggleSetting = () => {
       break;
   }
 
-  localStorage.setItem(STORAGE_KEY, currentSetting);
+  localStorage.setItem(COLOR_STORAGE_KEY, currentSetting);
 
   return currentSetting;
 };
 
-document.getElementById('themeToggle').addEventListener('click', evt => {
+document.getElementById('themeToggle').addEventListener('click', (evt) => {
   evt.preventDefault();
   applySetting(toggleSetting());
 });
 
 applySetting();
+
+const FONT_STORAGE_KEY = 'user-font-choice';
+const FONT_MODE_KEY = '--font-choice';
+const fontToggle = document.getElementById('fontToggle');
+
+const applyFontSetting = (passedSetting) => {
+  let currentSetting = passedSetting || localStorage.getItem(FONT_STORAGE_KEY);
+  if (currentSetting) {
+    document.documentElement.setAttribute('data-user-font', currentSetting);
+  }
+};
+
+const toggleFontSetting = () => {
+  let currentSetting = localStorage.getItem(FONT_STORAGE_KEY);
+
+  switch (currentSetting) {
+    case null:
+      currentSetting =
+        getCSSCustomProp(FONT_MODE_KEY) === 'dyslexic' ? 'default' : 'dyslexic';
+      break;
+    case 'default':
+      currentSetting = 'dyslexic';
+      break;
+    case 'dyslexic':
+      currentSetting = 'default';
+      break;
+  }
+
+  localStorage.setItem(FONT_STORAGE_KEY, currentSetting);
+
+  return currentSetting;
+};
+
+document.getElementById('fontToggle').addEventListener('click', (evt) => {
+  evt.preventDefault();
+  applyFontSetting(toggleFontSetting());
+});
+
+applyFontSetting();
