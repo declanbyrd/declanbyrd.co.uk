536f508f9bbe7c5845a7433e161af3e04853c932
Declan Byrd <declan.byrd@gmail.com>
2022-12-22 10:18:58 +0000

    refactor: book data and shortcode removal
    - fetch book data in eleventyComputed rather than shortcode.
    - removes duplication of data.
    - resolve timeout errors.

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ const inclusiveLangPlugin = require('@11ty/eleventy-plugin-inclusive-language');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const imageShortcode = require('./src/shortcodes/image');
 const cloudImageShortcode = require('./src/shortcodes/cloudImage');
-const bookShortcode = require('./src/shortcodes/book');
 const nowReading = require('./src/shortcodes/nowReading');
 const filters = require('./src/_filters/filters');
 const collections = require('./src/_filters/collections');
@@ -18,7 +17,6 @@ module.exports = (eleventyConfig) => {
 
   // Shortcodes
 
-  eleventyConfig.addNunjucksAsyncShortcode('book', bookShortcode);
   eleventyConfig.addNunjucksAsyncShortcode('nowReading', nowReading);
   eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
   eleventyConfig.addNunjucksAsyncShortcode('cloudImage', cloudImageShortcode);
