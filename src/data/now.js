df1785ea254a7d238314ed08b02e1af681fde812
Declan Byrd <declan.byrd@gmail.com>
2023-01-12 17:21:08 +0000

    chore: update now reading

--- src/data/now.js
+++ src/data/now.js
@@ -1,21 +1,6 @@
 const { getBook } = require('../utils');
 
-const BOOK = '0321965515';
-
-const data = {
-  work: {
-    role: 'software engineer',
-    company: {
-      name: 'Twilio',
-      website: 'https://twilio.com',
-    },
-  },
-  reading: '0321965515',
-  gaming: {
-    game: 'Spider-man',
-    platform: 'Playstation 5',
-  },
-};
+const BOOK = '9781804910184';
 
 module.exports = async () => {
   return {
