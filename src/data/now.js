a0b6de7a8f141edf1d0f4102d21e2c494512fb1c
Declan Byrd <declan.byrd@gmail.com>
2023-01-20 22:58:25 +0000

    now: update now reading

--- src/data/now.js
+++ src/data/now.js
@@ -1,9 +1,10 @@
 const { getBook } = require('../utils');
 
-const BOOK = '9781804910269';
+const BOOK = '9781804910245';
+const PAGES = 200;
 
 module.exports = async () => {
   return {
-    reading: await getBook(BOOK),
+    reading: await getBook(BOOK, PAGES),
   };
 };
