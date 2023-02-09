c6497ba1079706f87c31645207a85c488a55496c
Declan Byrd <declan.byrd@gmail.com>
2023-02-09 17:39:05 +0000

    refactor: remove renderData and use computed data
    - renderData deprecated in 11ty 2.0

--- src/content/bookshelf/bookshelf.11tydata.js
+++ src/content/bookshelf/bookshelf.11tydata.js
@@ -5,5 +5,8 @@ module.exports = {
   permalink: '/reading/{{page.fileSlug}}/',
   eleventyComputed: {
     book: async (data) => await getBook(data.isbn, data.pages),
+    title: (data) => data.book.title,
+    description: (data) =>
+      `Declan's notes for ${data.book.title} by ${data.book.authors}`,
   },
 };
