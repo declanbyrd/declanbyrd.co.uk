46f349d068fc6bdbf6d567748b0267d75312ac9c
Declan Byrd <declan.byrd@gmail.com>
2023-04-06 20:06:21 +0100

    refactor: remove references to renderData
    - renderData removed in 11ty 2.0
    - replace it with computedData
    - move some template logic into computedData
    - make sure all pages have title and description meta

--- src/content/photos/photos.11tydata.js
+++ src/content/photos/photos.11tydata.js
@@ -1,7 +1,17 @@
+const pluralise = (string, data) => {
+  if (data.length > 1) {
+    return `${string}s`;
+  }
+  return string;
+};
+
 module.exports = {
   layout: 'layouts/photo.njk',
   permalink: '/photos/{{page.fileSlug}}/',
   eleventyComputed: {
+    intro: (data) => `${pluralise('Photo', data.photo)} from`,
     image: '{{ photo[0].url }}',
+    title: '{{intro}} {{date| readableDates}}',
+    description: "Declan's {{intro}} {{date| readableDates}}",
   },
 };
