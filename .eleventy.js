1e8800b5384b03d43ac30ea7403a7f503960ad8a
Declan Byrd <declan.byrd@gmail.com>
2023-06-19 20:19:30 +0100

    refactor: use cloudImage shortcode
    - empty alt text can be used for decorative images.
    - "book cover for..." and "game cover for..." not useful.
    - use cloudImage shortcode instead of custom implementations.

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,6 @@ const inclusiveLangPlugin = require('@11ty/eleventy-plugin-inclusive-language');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const imageShortcode = require('./src/shortcodes/image');
 const cloudImageShortcode = require('./src/shortcodes/cloudImage');
-const bookCoverShortcode = require('./src/shortcodes/bookCover');
-const gameCoverShortcode = require('./src/shortcodes/gameCover');
 const filters = require('./src/_filters/filters');
 const collections = require('./src/_filters/collections');
 const mastoArchive = require('eleventy-plugin-mastoarchive');
@@ -27,8 +25,6 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
   eleventyConfig.addNunjucksAsyncShortcode('cloudImage', cloudImageShortcode);
-  eleventyConfig.addNunjucksAsyncShortcode('bookCover', bookCoverShortcode);
-  eleventyConfig.addNunjucksAsyncShortcode('gameCover', gameCoverShortcode);
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
   // Filters
